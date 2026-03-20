f531a31542f73a269399be4184ac48d9dd0364a8
moreluck <luckykamshak@gmail.com>
2026-03-20 14:55:00 +0100

    feat: implement createGame contract call in stacksService

--- frontend/src/chess/services/stacksService.js
+++ frontend/src/chess/services/stacksService.js
@@ -1,6 +1,7 @@
 import { StacksMainnet, StacksTestnet } from '@stacks/network';
-import { showConnect } from '@stacks/connect';
-import { NETWORK } from '../stacksConstants';
+import { showConnect, openContractCall } from '@stacks/connect';
+import { uintCV, boolCV, AnchorMode, PostConditionMode } from '@stacks/transactions';
+import { NETWORK, CONTRACTS } from '../stacksConstants';
 
 const network = NETWORK === 'mainnet' ? new StacksMainnet() : new StacksTestnet();
 
@@ -20,6 +21,28 @@ const stacksService = {
         name: 'Stackchess',
         icon: window.location.origin + '/vite.svg',
       },
+      onFinish,
+      onCancel,
+    });
+  },
+
+  /**
+   * Creates a new game on-chain
+   * @param {number} wager - Wager amount in micro-STX or token units
+   * @param {boolean} isStx - Whether the wager is in STX (true) or CHESS (false)
+   * @param {Object} callbacks - onFinish and onCancel callbacks
+   */
+  createGame: (wager, isStx, onFinish, onCancel) => {
+    const [contractAddress, contractName] = CONTRACTS.GAME.split('.');
+    
+    openContractCall({
+      contractAddress,
+      contractName,
+      functionName: 'create-game',
+      functionArgs: [uintCV(wager), boolCV(isStx)],
+      network,
+      anchorMode: AnchorMode.Any,
+      postConditionMode: PostConditionMode.Allow,
       onFinish,
       onCancel,
     });
